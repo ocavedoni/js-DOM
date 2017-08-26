9e653da9f1ad179811b9af1974edbb65e1960e7f
Olina Cavedoni <Olina@Olinas-MacBook-Pro.local>
2017-08-25 18:18:09 -1000

    tay

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@ Find the div with the id of "name1" and replace the n/a with the following:
 
 Tay-Tay*/
 
+var taylor = "Tay-Tay";
+
+var changeName = document.getElementById("name1");
+changeName.innerHTML = taylor;
+
 
 /*2. DJ Khaled
 
